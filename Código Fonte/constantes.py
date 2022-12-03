577a613b4a9a6923bdfbd64a9d923e9cc4c9017b
Your Name <youremail@domain.com>
2022-12-03 20:35:38 -0300

    Pastas atualizadas.

--- Código Fonte/constantes.py
+++
@@ -1,6 +0,0 @@
-# Principais constantes do nosso jogo
-
-cores_gemas = ['A','B','C','D','E','F','G','H','I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-tabuleiro = []
-cores_escolhidas = []
-junt1 = ""
