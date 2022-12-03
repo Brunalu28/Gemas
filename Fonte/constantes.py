a0e7cee8525575464e4be504789a2b1a74d29687
Your Name <youremail@domain.com>
2022-12-03 20:22:09 -0300

    Atualização das constantes e adição das instruções do jogo e apresentação do tabuleiro.

---
+++ Fonte/constantes.py
@@ -0,0 +1,22 @@
+# Principais constantes do nosso jogo
+
+NIVEL_FACIL = "    0  1  2 "
+LINHAFACIL = "+------------+"
+NIVEL_MEDIO = "    0  1  2  3  4  5 "
+LINHAMEDIO = "+---------------------+"
+NIVEL_DIFICIL = "    0  1  2  3  4  5  6  7  8 "
+LINHADIFICIL = "+------------------------------+"
+
+cores_gemas = ['A','B','C','E','K', 'L', 'M', 'V', 'W', 'Y']
+
+INSTRUCOES = """
+
+ALGUNS AVISOS IMPORTANTES PARA O JOGO:
+
+T - PARA REALIZAR A TROCA DE GEMAS;
+D - PARA DICAS NO JOGO (PERDE 1 PONTO);
+S - PARA SAIR DO JOGO.
+
+"""
+
+
