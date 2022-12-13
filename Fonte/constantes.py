3314a000aed93717af3b21bcc85bd6197e838190
Your Name <youremail@domain.com>
2022-12-13 19:11:44 -0300

    Novos ajustes.

--- Fonte/constantes.py
+++ Fonte/constantes.py
@@ -1,16 +1,11 @@
 import sys
 # Principais constantes do nosso jogo
 
-NIVEL_FACIL = "    0  1  2 "
-LINHAFACIL = "+------------+"
-NIVEL_MEDIO = "    0  1  2  3  4  5 "
-LINHAMEDIO = "+---------------------+"
-NIVEL_DIFICIL = "    0  1  2  3  4  5  6  7  8 "
-LINHADIFICIL = "+------------------------------+"
+
 
 gemascoloridas = {"AZUL": "\033[34m", "BRANCO": "\033[37m", "CIANETO": "\033[36m", "AMARELO": "\033[33m", "MAGENTA": "\033[35m", "VERDE": "\033[32m", "VERMELHO": "\033[31m",}
 
-cores_gemas = ['A','B','C','E','K', 'L', 'M', 'V', 'W', 'Y']
+cores_gemas = ['A','B','C','D','E','F','G','H','I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
 
 
